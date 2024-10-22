7f8ff5222626996e6424b646d4f8aa004991c41d
henricaioo <12201000103@muz.ifsuldeminas.edu.br>
2024-10-22 16:55:50 -0300

    primeiro commit

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length = 50)
+    fabricante = models.CharField(max_length = 50)
+    imagem = models.ImageField(upload_to = 'static/', blank = True)
+    preco = models.DecimalField(max_digits = 10, decimal_places = 2)
+    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.nome
